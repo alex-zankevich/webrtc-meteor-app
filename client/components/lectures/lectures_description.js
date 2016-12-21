9c766dac025826c1d416332357135e5327a41fa4
Aliaksandr Zankevich <aliaksandr.zankevich@gmail.com>
2016-12-21 19:54:29 +0300

    refactor lecture_desciption

--- client/components/lectures/lectures_description.js
+++ client/components/lectures/lectures_description.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
-const DescriptionInfo = ({ name, description }) => {
+export const DescriptionInfo = ({ name, description, owner }) => {
 	return (
 		<div className="description-info">
-			<h4>Name: {name}</h4>
-			<h4>Description: {description}</h4>
+			<h3>{ name }</h3>
+			<h4>{ description }</h4>
+			{Meteor.userId() === owner &&
+			<button className="btn btn-default toggle-description"
+							data-toggle="collapse"
+							data-target=".description-wrapper">Edit descriptioin
+			</button>}
 		</div>
 	);
 };
 
-export default class LecturesDescription extends Component {
+export class DescriptionEdit extends Component {
 	constructor(props) {
 		super(props);
 
@@ -30,43 +35,42 @@ export default class LecturesDescription extends Component {
 		Meteor.call('lectures.updateDescription', this.props.lecture, updatedValue);
 	}
 
-	renderEditDescription() {
-		if (Meteor.user()._id === this.props.lecture.ownerId) {
-			return (
+	render() {
+		return (
+			<div className="description-wrapper collapse">
+				<div className="form-group">
+					<label htmlFor="lecture-name">Name:</label>
+					<input type="text"
+								 className="form-control"
+								 id="lecture-name"
+								 ref="lectureName" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="lecture-description">Short description:</label>
+					<input type="text"
+								 className="form-control"
+								 id="lecture-description"
+								 ref="lectureDescription" />
+				</div>
 				<button className="btn btn-default toggle-description"
 								data-toggle="collapse"
-								data-target=".description-wrapper">Edit descriptioin</button>
-			);
-		}
+								data-target=".description-wrapper"
+								onClick={ this.handleChange.bind(this) }>
+					Update
+				</button>
+			</div>
+		);
 	}
+};
 
+export default class LecturesDescription extends Component {
 	render() {
 		return (
 			<div className="lectures-description-container">
-				<DescriptionInfo name={ this.state.name } description={ this.state.description } />
-				{ this.renderEditDescription() }
-				<div className="description-wrapper collapse">
-					<div className="form-group">
-						<label htmlFor="lecture-name">Name:</label>
-						<input type="text"
-									 className="form-control"
-									 id="lecture-name"
-									 ref="lectureName" />
-					</div>
-					<div className="form-group">
-						<label htmlFor="lecture-description">Short description:</label>
-						<input type="text"
-									 className="form-control"
-									 id="lecture-description"
-									 ref="lectureDescription" />
-					</div>
-					<button className="btn btn-default toggle-description"
-									data-toggle="collapse"
-									data-target=".description-wrapper"
-									onClick={ this.handleChange.bind(this) }>
-						Update
-					</button>
-				</div>
+				<DescriptionInfo name={ this.props.lecture.name }
+												 description={ this.props.lecture.description }
+												 owner={ this.props.lecture.ownerId }/>
+				<DescriptionEdit lecture={ this.props.lecture }/>
 			</div>
 		);
 	}
