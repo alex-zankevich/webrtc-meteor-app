b25ed6cf9d5db13de84d635c44ce7264b0367bc4
Aliaksandr Zankevich <aliaksandr.zankevich@gmail.com>
2016-12-21 21:46:02 +0300

    chat refactor

--- client/components/lectures/lectures_chat.js
+++ client/components/lectures/lectures_chat.js
@@ -9,50 +9,35 @@ class LecturesChat extends Component {
 	}
 
 	renderIcon(message) {
-		if (this.isUserMessage(message)) {
-			return (
-        <span className="chat-img pull-right">
-					<img src="http://placehold.it/50/FA6F57/fff&amp;text=ME" alt="User Avatar" className="img-circle" />
-				</span>
-      );
-		} else {
-			return (
-				<span className="chat-img pull-left">
-					<img src="http://placehold.it/50/55C1E7/fff&amp;text=U" alt="User Avatar" className="img-circle" />
-				</span>
-			);
-		}
+		return (
+      <span className={`chat-img ${this.isUserMessage(message) ? 'pull-right' : 'pull-left'}`}>
+				<img src={`/avatar_default/${this.isUserMessage(message) ? 'red' : 'blue'}/avatar-extrasmall.png`}
+						 alt="User Avatar"
+						 className="img-circle" />
+			</span>
+    );
 	}
 
 	renderHeader(message) {
-		if (this.isUserMessage(message)) {
-			return (
-        <div className="header">
-        	<small className="text-muted">
-						<span className="glyphicon glyphicon-time"></span>{message.createdAt.toUTCString()}
-					</small>
-					<strong className="pull-right primary-font">{this.getEmail(message.user ? message.user.emails[0] : '')}</strong>
-				</div>
-      );
-		} else {
-			return (
-				<div className="header">
-					<strong className="primary-font">{this.getEmail(message.user ? message.user.emails[0] : '')}</strong>
-					<small className="pull-right text-muted">
-						<span className="glyphicon glyphicon-time"></span>{message.createdAt.toUTCString()}
-					</small>
-				</div>
-			);
-		}
+		return (
+      <div className="header">
+      	<small className={`${this.isUserMessage(message) ? '' : 'pull-right'} text-muted`}>
+					<span className="glyphicon glyphicon-time"></span>{message.createdAt.toUTCString()}
+				</small>
+				<strong className={`${this.isUserMessage(message) ? 'pull-right' : ''} primary-font`}>
+					{this.getEmail(message.user ? message.user.emails[0] : '')}
+				</strong>
+			</div>
+    );
 	}
 
 	renderMessages() {
 		return this.props.messages.map(message => (
-			<li className={(this.isUserMessage(message) ? "right" : "left") + " clearfix"} key={message._id}>
+			<li className={`${this.isUserMessage(message) ? 'right' : 'left'} clearfix`} key={ message._id }>
 				{ this.renderIcon(message) }
 				<div className="chat-body clearfix">
 					{ this.renderHeader(message) }
-					<p>{message.message}</p>
+					<p>{ message.message }</p>
 				</div>
 			</li>
 		));
@@ -65,39 +50,34 @@ class LecturesChat extends Component {
 	sendMessage() {
 		let message;
 
-		if (this.refs.messageInput && (message = this.refs.messageInput.value)) {
-			return Meteor.call('messages.insert', this.props.lecture, message, Meteor.user());
+		if (this.refs.messageInput && (message = this.refs.messageInput.value) && message) {
+			Meteor.call('messages.insert', this.props.lecture, message, Meteor.user());
+			this.refs.messageInput.value = '';
 		}
 	}
 
 	render() {
 		return (
 			<div className="container chat-wrapper">
-				<div className="row">
-						<div className="col-md-6 col-lg-6 col-xs-12">
-								<div className="panel panel-primary">
-										<div className="panel-body">
-												<ul className="chat">
-														{ this.renderMessages() }
-												</ul>
-										</div>
-										<div className="panel-footer">
-												<div className="input-group">
-														<input id="btn-input"
-																	 type="text"
-																	 className="form-control input-sm"
-																	 ref="messageInput"
-																	 placeholder="Type your message here..." />
-														<span className="input-group-btn">
-																<button className="btn btn-primary btn-sm"
-																				id="btn-chat"
-																				onClick={ this.sendMessage.bind(this) }>
-																		Send
-																</button>
-														</span>
-												</div>
-										</div>
-								</div>
+				<div className="panel panel-primary col-md-6 col-lg-6 col-xs-12">
+						<div className="panel-body">
+								<ul className="chat">
+										{ this.renderMessages() }
+								</ul>
+						</div>
+						<div className="panel-footer input-group">
+							<input id="btn-input"
+										 type="text"
+										 className="form-control input-sm"
+										 ref="messageInput"
+										 placeholder="Type your message here..." />
+							<span className="input-group-btn">
+									<button className="btn btn-primary btn-sm"
+													id="btn-chat"
+													onClick={ this.sendMessage.bind(this) }>
+											Send
+									</button>
+							</span>
 						</div>
 				</div>
 		</div>
